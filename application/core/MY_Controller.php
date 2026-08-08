680a91452910035f76e3dfa4101678b652c75573
Rahmat Triadi <rahmat_triadi@yahoo.com>
2026-08-08 16:33:56 +0800

    fix: set default PHP timezone to Asia/Makassar (WITA) and sync MySQL session time_zone to +08:00

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -13,6 +13,9 @@ class MY_Controller extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        if (isset($this->db) && $this->db->conn_id) {
+            $this->db->query("SET time_zone = '+08:00'");
+        }
         $this->_check_auth();
     }
 
